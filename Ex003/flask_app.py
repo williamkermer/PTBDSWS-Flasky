abcd6b61996dfbc84fbd1d05ebd4e349da08976d
William Kermer <William.Kermer21@gmail.com>
2025-08-26 11:44:34 -0300

    Aula 03

---
+++ Ex003/flask_app.py
@@ -0,0 +1,37 @@
+#Exercicio 3
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+# Rota 1: Página Inicial
+@app.route("/")
+def home():
+    """Renderiza a página inicial com a data e hora atuais."""
+    return render_template('index.html', current_time=datetime.utcnow())
+
+# Rota 2: Perfil do Usuário
+@app.route('/user/<username>')
+def user_profile(username):
+    """Renderiza uma página de perfil para um usuário específico."""
+    return render_template('user.html', name=username)
+
+# Rota 3 (Opcional): Uma rota específica para a página de erro
+@app.route('/rotainexistente')
+def not_found_route():
+    """Uma rota definida que mostra a página de erro 404."""
+    return render_template('404.html'), 404
+
+# Manipulador de Erro 4: Para qualquer outra rota não encontrada
+@app.errorhandler(404)
+def page_not_found(e):
+    """Manipulador de erro para páginas não encontradas (404)."""
+    return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)

a39f0fb5c7b33d6afc6ebe02e5793cbae649eb37
William Kermer <William.Kermer21@gmail.com>
2025-11-28 21:15:58 -0300

    Avaliação-Semestral

--- Avaliação-Semestral/app/main/forms.py
+++ Avaliação-Semestral/app/main/forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField 
 from wtforms.validators import DataRequired
 
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+    
+
+class ProfessorForm(FlaskForm):
+    name = StringField('Cadastre o novo Professor:', validators=[DataRequired()])
+    role = SelectField('Disciplina associada:', choices=[
+        ('DSWA5', 'DSWA5'),
+        ('GPSA5', 'GPSA5'),
+        ('IHCA5', 'IHCA5'),
+        ('SODA5', 'SODA5'),
+        ('PJIA5', 'PJIA5'),
+        ('TCOA5', 'TCOA5')
+    ])
+    submit = SubmitField('Cadastrar')
--- Avaliação-Semestral/app/main/views.py
+++ Avaliação-Semestral/app/main/views.py
@@ -1,31 +1,51 @@
-from flask import render_template, session, redirect, url_for, current_app
-from .. import db
-from ..models import User
-from ..email import send_email, send_simple_message
+# 1. Adicionei 'flash' nas importações
+from flask import render_template, redirect, url_for, flash
+from datetime import datetime
 from . import main
-from .forms import NameForm
+from .forms import NameForm, ProfessorForm
+from .. import db
+from ..models import Role, User
 
-
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    form = NameForm()
+    return render_template('index.html', current_time=datetime.utcnow())
+
+@main.route('/professores', methods=['GET', 'POST'])
+def professores():
+    form = ProfessorForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-            print('FLASKY_ADMIN: ' + str(current_app.config['FLASKY_ADMIN']), flush=True)
-            if current_app.config['FLASKY_ADMIN']:
-                #send_email(current_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
-                print('Enviando mensagem...', flush=True)
-                send_simple_message([current_app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"], 'Novo usuário', form.name.data)
-                print('Mensagem enviada...', flush=True)
+        # Verifica se o Professor JÁ EXISTE
+        professor_existente = User.query.filter_by(username=form.name.data).first()
+        
+        if professor_existente:
+            # 2. Mensagem de Erro/Aviso
+            flash('Professor já existe na base de dados!')
         else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False))
+            # Lógica da Disciplina (Role)
+            disciplina_nome = form.role.data
+            role = Role.query.filter_by(name=disciplina_nome).first()
+            
+            if role is None:
+                role = Role(name=disciplina_nome)
+                db.session.add(role)
+                db.session.commit()
+            
+            # Cria o novo Professor
+            professor = User(username=form.name.data, role=role)
+            db.session.add(professor)
+            db.session.commit()
+            
+            # 3. Mensagem de Sucesso
+            flash('Professor cadastrado com sucesso!')
+        
+        return redirect(url_for('.professores'))
+    
+    professores_list = User.query.order_by(User.username).all()
+    
+    return render_template('professores.html', 
+                           form=form, 
+                           professores=professores_list)
+                           
+@main.route('/indisponivel')
+def unavailable():
+    return render_template('unavailable.html', current_time=datetime.utcnow())
